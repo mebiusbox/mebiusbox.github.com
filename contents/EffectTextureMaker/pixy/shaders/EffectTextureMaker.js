a5b42404af33f2fc15b1ea5b7ce906cac0cd5d07
mebiusbox <flare.mebius@gmail.com>
2017-01-07 15:46:04 +0900

    EffectTextureMaker: 爆発２追加

--- contents/EffectTextureMaker/pixy/shaders/EffectTextureMaker.js
+++ contents/EffectTextureMaker/pixy/shaders/EffectTextureMaker.js
@@ -2407,6 +2407,235 @@ PixSpriteStudioShaderChunks = {
   ].join("\n"),
   
   
+  
+  // port from https://www.shadertoy.com/view/lsySzd
+  // "Volumetric explosion" by Duke
+  //-------------------------------------------------------------------------------------
+  // Based on "Supernova remnant" (https://www.shadertoy.com/view/MdKXzc) 
+  // and other previous shaders 
+  // otaviogood's "Alien Beacon" (https://www.shadertoy.com/view/ld2SzK)
+  // and Shane's "Cheap Cloud Flythrough" (https://www.shadertoy.com/view/Xsc3R4) shaders
+  // Some ideas came from other shaders from this wonderful site
+  // Press 1-2-3 to zoom in and zoom out.
+  // License: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+  //-------------------------------------------------------------------------------------
+  
+  explosion2Uniforms: {
+    explosionCameraPan: { value: 0.0 },
+    explosionSpeed: { value: 1.0 },
+    explosionDensity: { value: 1.0 },
+    explosionEmission: { value: 0.2 },
+    explosionBloom: { value: 0.0 },
+    explosionColor: { value: 1.0 },
+  },
+  
+  explosion2FragPars: [
+    "uniform float explosionCameraPan;",
+    "uniform float explosionSpeed;",
+    "uniform float explosionDensity;",
+    "uniform float explosionEmission;",
+    "uniform float explosionBloom;",
+    "uniform float explosionColor;",
+    
+    // "#define DITHERING",
+    // "#define TOENMAPPING",
+    
+    "#define R(p, a) p=cos(a)*p + sin(a)*vec2(p.y,-p.x)",
+    
+    "vec3 hash(vec3 p) {",
+    "  p = vec3(dot(p, vec3(127.1, 311.7, 74.7)),",
+    "           dot(p, vec3(269.5, 183.3, 246.1)),",
+    "           dot(p, vec3(113.5, 271.9, 124.6)));",
+    "  return -1.0 + 2.0 * fract(sin(p)*43758.5453123);",
+    "}",
+    
+    "float expNoise(in vec3 p) {",
+    "  vec3 i = floor(p);",
+    "  vec3 f = fract(p);",
+    "  vec3 u = f*f*(3.0-2.0*f);",
+    "  return mix(mix(mix(dot(hash(i + vec3(0.0,0.0,0.0)), f-vec3(0.0,0.0,0.0)),",
+    "                     dot(hash(i + vec3(1.0,0.0,0.0)), f-vec3(1.0,0.0,0.0)), u.x),",
+    "                 mix(dot(hash(i + vec3(0.0,1.0,0.0)), f-vec3(0.0,1.0,0.0)),",
+    "                     dot(hash(i + vec3(1.0,1.0,0.0)), f-vec3(1.0,1.0,0.0)), u.x), u.y),",
+    "             mix(mix(dot(hash(i + vec3(0.0,0.0,1.0)), f-vec3(0.0,0.0,1.0)),",
+    "                     dot(hash(i + vec3(1.0,0.0,1.0)), f-vec3(1.0,0.0,1.0)), u.x),",
+    "                 mix(dot(hash(i + vec3(0.0,1.0,1.0)), f-vec3(0.0,1.0,1.0)),",
+    "                     dot(hash(i + vec3(1.0,1.0,1.0)), f-vec3(1.0,1.0,1.0)), u.x), u.y), u.z);",
+    "}",
+    
+    "float fbm(vec3 p) {",
+    "  return expNoise(p*0.6125)*0.5 + expNoise(p*0.125)*0.25 + expNoise(p*0.25)*0.125 + expNoise(p*0.4)*0.2;",
+    "}",
+    
+    "float sphere(vec3 p, float r) {",
+    "  return length(p)-r;",
+    "}",
+    
+    //==============================================================
+    // otaviogood's noise from https://www.shadertoy.com/view/ld2SzK
+    //--------------------------------------------------------------
+    // This spiral noise works by successively adding and rotating sin waves while increasing frequency.
+    // It should work the same on all computers since it's not based on a hash function like some other noises.
+    // It can be much faster than other noise functions if you're ok with some repetition.
+    "const float nudge = 4.0;", // size of perpendicular vector
+    "float normalizer = 1.0 / sqrt(1.0 + nudge*nudge);", // pythagorean theorem on that perpendicular to maintain scale
+    "float spiralNoiseC(vec3 p) {",
+    "  float n = -mod(time * 0.8 * explosionSpeed, -2.1);", // noise amount
+    "  float iter = 2.0;",
+    "  for (int i=0; i<8; i++) {",
+         // add sin and cos scaled inverse with the frequency
+    "    n += -abs(sin(p.y*iter) + cos(p.x*iter)) / iter;", // abs for a ridged look
+         // rotate by adding perpendicular and scaling down
+    "    p.xy += vec2(p.y, -p.x) * nudge;",
+    "    p.xy *= normalizer;",
+         // rotate on other axis
+    "    p.xz += vec2(p.z, -p.x) * nudge;",
+    "    p.xz *= normalizer;",
+         // increase the frequency
+    "    iter *= 1.733733;",
+    "  }",
+    "  return n;",
+    "}",
+    
+    "float volumetricExplosion(vec3 p) {",
+    "  float final = sphere(p, 4.0);",
+    // "  final += expNoise(p*12.5)*0.2;",
+    "  final += fbm(p*50.0 * explosionDensity);",
+    "  final += spiralNoiseC(p.zxy*0.4132+333.0)*3.0;", // 1.25
+    "  return final;",
+    "}",
+    
+    "float map(vec3 p) {",
+    // "  R(p.xz, mouse.x * 0.008 * PI * time*0.1);",
+    "  R(p.xz, explosionCameraPan * PI2);",
+    "  return volumetricExplosion(p/0.6)*0.6;", // scale
+    "}",
+    
+    // assing color to the media
+    "vec3 computeColor(float density, float radius) {",
+    // color based on density alone, gives impresion of occlusion within the media
+    "  vec3 result = mix(vec3(1.0, 0.9, 0.8), vec3(0.4, 0.15, 0.1), density);",
+    
+    // color added to the media
+    "  vec3 colCenter = 7.0 * vec3(0.8, 1.0, 1.0);",
+    "  vec3 colEdge = 1.5 * vec3(0.48, 0.53, 0.5);",
+    "  result *= mix(colCenter, colEdge, min((radius + 0.05)/0.9, 1.15));",
+    "  return result;",
+    "}",
+    
+    "bool raySphereIntersect(vec3 org, vec3 dir, out float near, out float far) {",
+    "  float b = dot(dir, org);",
+    "  float c = dot(org, org) - 8.0;",
+    "  float delta = b*b - c;",
+    "  if (delta < 0.0) return false;",
+    "  float deltasqrt = sqrt(delta);",
+    "  near = -b - deltasqrt;",
+    "  far = -b + deltasqrt;",
+    "  return far > 0.0;",
+    "}",
+    
+    // Applies the filmic curve from John Hable's presentation
+    // More details at : http://filmicgames.com/archives/75
+    "vec3 toneMapFilmicALU(vec3 color) {",
+    "  color = max(vec3(0), color - vec3(0.004));",
+    "  color = (color * (6.2*color + vec3(0.5))) / (color * (6.2 * color + vec3(1.7)) + vec3(0.06));",
+    "  return color;",
+    "}",
+    
+  ].join("\n"),
+  
+  explosion2Frag: [
+    
+    // ro: ray origin
+    // rd: direction of the ray
+    "vec3 rd = normalize(vec3((pin.coord.xy - 0.5*resolution.xy) / resolution.y, 1.0));",
+    "vec3 ro = vec3(0.0, 0.0, -6.0);",
+    
+    "#ifdef DITHERING",
+    "vec2 seed = pin.uv + fract(time);",
+    "#endif",
+    
+    // ld, td: local, total density
+    // w: weighting factor
+    "float ld = 0.0, td = 0.0, w = 0.0;",
+    
+    // t: length of the ray
+    // d: distance function
+    "float d = 1.0, t = 0.0;",
+    
+    "const float h = 0.1;",
+    "vec4 sum = vec4(0.0);",
+    "float min_dist = 0.0, max_dist = 0.0;",
+    
+    "if (raySphereIntersect(ro, rd, min_dist, max_dist)) {",
+    "  t = min_dist * step(t, min_dist);",
+    "  for (int i=0; i<86; i++) {",
+    
+    "    vec3 pos = ro + t*rd;",
+    
+    "    if (td > 0.9 || d < 0.12*t || t > 10.0 || sum.a > 0.99 || t > max_dist) break;",
+    
+    "    float d = map(pos);",
+    "    d = abs(d) + 0.07;",
+         // change this starting to control density
+    "    d = max(d, 0.03);",
+    
+         // point light calculations
+    "    vec3 ldst = vec3(0.0) - pos;",
+    "    float lDist = max(length(ldst), 0.001);",
+    
+         // the color of light
+    "    vec3 lightColor = vec3(1.0, 0.5, 0.25);",
+    // "    sum.rgb += (lightColor / exp(lDist*lDist*lDist*0.08)/30.0);",// bloom
+    "    sum.rgb += (lightColor / exp(lDist*lDist*lDist*0.15)/(30.0 - 20.0 * explosionBloom));",// bloom
+    
+    "    if (d < h) {",
+           // compute local density
+    "      ld = h - d;",
+           // compute weighting factor
+    "      w = (1.0 - td) * ld;",
+           // accumulate density
+    "      td += w + 1.0 / 200.0;",
+    
+    "      vec4 col = vec4(computeColor(td, lDist), td);",
+           // emission
+    "      sum += sum.a * vec4(sum.rgb, 0.0) * explosionEmission / lDist;",
+           // uniform scale density
+    "      col.a *= 0.2;",
+           // colour by alpha
+    "      col.rgb *= col.a;",
+           // alpha blend in contribution
+    "      sum = sum + col*(1.0 - sum.a);",
+    "    }",
+    
+    "    td += 1.0 / 70.0;",
+    
+    "    #ifdef DITHERING",
+    "    d = abs(d) * (0.8 + 0.2*rand(seed*vec2(i)*0.123));",
+    "    #endif",
+         // trying to optimize step size
+    // "    t += max(d*0.25, 0.01);",
+    "    t += max(d * 0.08 * max(min(length(ldst), d), 2.0), 0.01);",
+    "  }",
+    // simple scattering
+    // "  sum *= 1.0 / exp(ld * 0.2) * 0.9;",
+    "  sum *= 1.0 / exp(ld * 0.2) * 0.8;",
+    "  sum = clamp(sum, 0.0, 1.0);",
+    "  sum.xyz = sum.xyz * sum.xyz * (3.0 - 2.0 * sum.xyz);",
+    "}",
+    
+    "vec3 gray = vec3(rgb2gray(sum.xyz));",
+    "sum.xyz = mix(gray, sum.xyz, explosionColor);",
+    
+    "#ifdef TOENMAPPING",
+    "pout.color = toneMapFilmicALU(sum.xyz*2.02);",
+    "#else",
+    "pout.color = sum.xyz;",
+    "#endif",
+
+  ].join("\n"),
+  
+  
   //// CORONA
   // https://www.shadertoy.com/view/XdV3DW by vamoss
   
@@ -3006,9 +3235,9 @@ PixSpriteStudioShaderChunks = {
 
   
   //// TEST CHUNK
-  
+
   testFragPars: [
-    
+  
   ].join("\n"),
   
   testFrag: [
@@ -3216,6 +3445,7 @@ PixSpriteStudioShader = function() {
     this.addUniform(uniforms, ["MAGICCIRCLE"], "magicCircleUniforms");
     this.addUniform(uniforms, ["CROSS"], "crossUniforms");
     this.addUniform(uniforms, ["EXPLOSION"], "explosionUniforms");
+    this.addUniform(uniforms, ["EXPLOSION2"], "explosion2Uniforms");
     this.addUniform(uniforms, ["CORONA"], "coronaUniforms");
     this.addUniform(uniforms, ["LENSFLARE"], "lensFlareUniforms");
     this.addUniform(uniforms, ["SUN"], "sunUniforms");
@@ -3280,6 +3510,7 @@ PixSpriteStudioShader = function() {
     this.addCode(codes, ["MAGICCIRCLE"], "magicCircleFragPars");
     this.addCode(codes, ["CROSS"], "crossFragPars");
     this.addCode(codes, ["EXPLOSION"], "explosionFragPars");
+    this.addCode(codes, ["EXPLOSION2"], "explosion2FragPars");
     this.addCode(codes, ["CORONA"], "coronaFragPars");
     this.addCode(codes, ["LENSFLARE"], "lensFlareFragPars");
     this.addCode(codes, ["SUN"], "sunFragPars");
@@ -3327,6 +3558,7 @@ PixSpriteStudioShader = function() {
       this.addCode(codes, ["MAGICCIRCLE"], "magicCircleFrag");
       this.addCode(codes, ["CROSS"], "crossFrag");
       this.addCode(codes, ["EXPLOSION"], "explosionFrag");
+      this.addCode(codes, ["EXPLOSION2"], "explosion2Frag");
       this.addCode(codes, ["CORONA"], "coronaFrag");
       this.addCode(codes, ["LENSFLARE"], "lensFlareFrag");
       this.addCode(codes, ["SUN"], "sunFrag");
